88ebae2149f063b5dd52ce315875bb83784c0e39
LogeshwaranRAJAGOPAL <100465267+LogeshwaranRAJAGOPAL@users.noreply.github.com>
2022-07-29 00:04:32 +0530

    Add files via upload

---
+++ src/main/java/com/sample/demoWebApp/Repository/repository.java
@@ -0,0 +1,9 @@
+package com.sample.demoWebApp.Repository;
+
+import com.sample.demoWebApp.Model.Student;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface repository extends MongoRepository<Student, String> {
+}
