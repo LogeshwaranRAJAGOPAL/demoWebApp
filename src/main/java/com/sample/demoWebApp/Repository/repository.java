a766d4dce19ac841bce91cf5603d43ea62a44cf4
LogeshwaranRAJAGOPAL <100465267+LogeshwaranRAJAGOPAL@users.noreply.github.com>
2022-08-04 13:55:19 +0530

    Update repository.java

--- src/main/java/com/sample/demoWebApp/Repository/repository.java
+++ src/main/java/com/sample/demoWebApp/Repository/repository.java
@@ -1,9 +1,10 @@
 package com.sample.demoWebApp.Repository;
 
 import com.sample.demoWebApp.Model.Student;
-import org.springframework.data.mongodb.repository.MongoRepository;
+//import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 
 @Repository
-public interface repository extends MongoRepository<Student, String> {
+public interface repository extends JpaRepository<Student, String> {
 }
