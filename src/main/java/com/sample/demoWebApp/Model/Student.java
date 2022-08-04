88187642dba9e96ec8383e3af087178a23d98452
LogeshwaranRAJAGOPAL <100465267+LogeshwaranRAJAGOPAL@users.noreply.github.com>
2022-08-04 14:06:13 +0530

    Update Student.java

--- src/main/java/com/sample/demoWebApp/Model/Student.java
+++ src/main/java/com/sample/demoWebApp/Model/Student.java
@@ -6,7 +6,7 @@ import org.springframework.data.mongodb.core.mapping.Document;
 
 import javax.annotation.Generated;
 
-@Document(collection = "StudentDataBse")
+//@Document(collection = "StudentDataBse")
 public class Student {
 
 
